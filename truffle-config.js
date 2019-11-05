2ff378bf71f1e31241f24dc2c78a81a59b9e793b
haoli <lixx4002@umn.edu>
2019-11-04 22:24:33 -0800

    Cleaned up the truffle configuration file.

--- truffle-config.js
+++ truffle-config.js
@@ -21,8 +21,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
-// const infuraKey = "fj4jll3k.....";
-//
 
 module.exports = {
   /**
@@ -47,17 +45,7 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
-
-    // Another network with more advanced options...
-    // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
+	
 	rinkeby: {
 		provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/6aa29d6a396e4820891471316f5ceaac'),
 		from: "0xDC7c68458cf288b5367978469387cf0e9C5728fc", // default address to use for any transaction Truffle makes during migrations
@@ -65,12 +53,6 @@ module.exports = {
 		gas:      4544300,
 		gasPrice: 300000000000
 	} 
-	// Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -81,14 +63,12 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+		version: "0.5.7",    // Fetch exact version from solc-bin (default: truffle's version)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
       //    enabled: false,
       //    runs: 200
       //  },
-      //  evmVersion: "byzantium"
       // }
     }
   }
