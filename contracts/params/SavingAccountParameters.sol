9e7d82768c09764b0d5f78e3f3cee0d486542f5f
Bury Huang <burywong.definer@gmail.com>
2020-01-05 21:30:08 -0600

    update with latest contract

--- contracts/params/SavingAccountParameters.sol
+++ contracts/params/SavingAccountParameters.sol
@@ -6,26 +6,16 @@ contract SavingAccountParameters {
     address[] public tokenAddresses;
 
     constructor() public payable{
-        ratesURL = "json(https://api.rinkeby.definer.org/OKh4I2yYpKU8S2af/definer/api/v1.0/saving_pool/rates/current).[ETH,DAI,USDC,USDT,TUSD,PAX,GUSD,BNB,MKR,BAT,OMG,GNT,ZRX,REP,CRO,WBTC].USD";
-    	tokenNames = "ETH,DAI,USDC,USDT,TUSD,PAX,GUSD,BNB,MKR,BAT,OMG,GNT,ZRX,REP,CRO,WBTC";
+        ratesURL = "json(https://api.rinkeby.definer.org/OKh4I2yYpKU8S2af/definer/api/v1.0/saving_pool/rates/current).[ETH,DAI,USDT,TUSD,GUSD,FIN].USD";
+    	tokenNames = "ETH,DAI,USDT,TUSD,GUSD,FIN";
 
-		tokenAddresses = new address[](16);
+		tokenAddresses = new address[](6);
 		tokenAddresses[0] = 0x000000000000000000000000000000000000000E;
-		tokenAddresses[1] = 0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359;
-		tokenAddresses[2] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-		tokenAddresses[3] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-		tokenAddresses[4] = 0x0000000000085d4780B73119b644AE5ecd22b376;
-		tokenAddresses[5] = 0x8E870D67F660D95d5be530380D0eC0bd388289E1;
-		tokenAddresses[6] = 0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd;
-		tokenAddresses[7] = 0xB8c77482e45F1F44dE1745F52C74426C631bDD52;
-		tokenAddresses[8] = 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2;
-		tokenAddresses[9] = 0x0D8775F648430679A709E98d2b0Cb6250d2887EF;
-		tokenAddresses[10] = 0xd26114cd6EE289AccF82350c8d8487fedB8A0C07;
-		tokenAddresses[11] = 0xa74476443119A942dE498590Fe1f2454d7D4aC0d;
-		tokenAddresses[12] = 0xE41d2489571d322189246DaFA5ebDe1F4699F498;
-		tokenAddresses[13] = 0x1985365e9f78359a9B6AD760e32412f4a445E862;
-		tokenAddresses[14] = 0xA0b73E1Ff0B80914AB6fe0444E65848C4C34450b;
-		tokenAddresses[15] = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+		tokenAddresses[1] = 0x8c8c812ea7bb3c32B45645f7cbf84c7F902049d6;
+		tokenAddresses[2] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+		tokenAddresses[3] = 0x36e5aA7Ee4D004067eE3D0177F805269434DdC83;
+		tokenAddresses[4] = 0x388B0D6c519B1a502302f81a56EFeDA0B137d9c1;
+		tokenAddresses[5] = 0xaB8Ad2d7f39A54960Be8b71e01276a9E897833eE;
 	}
 
 	function getTokenAddresses() public view returns(address[] memory){
