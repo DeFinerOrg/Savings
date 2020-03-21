33f915ebaca0ae44581839d4d8cfd8d6ef959ef9
Sichao Yang <ysc@canonchain.com>
2020-03-21 13:01:19 -0400

    Removed a few merging conflicts left.

--- contracts/compound/CTokenRegistry.sol
+++ contracts/compound/CTokenRegistry.sol
@@ -11,42 +11,32 @@ contract CTokenRegistry is Ownable {
     event TokenRemoved(address indexed token);
 
     mapping(address => bool) public tokens;
-<<<<<<< HEAD
-    
-=======
 
     /**
      * @dev Add a new cToken to registry
      * @param _token cToken address
      */
->>>>>>> a983c67eff343846bd601e47d1453257c508091c
     function addToken(address _token) external onlyOwner {
         require(tokens[_token] == false, "Token already exist");
         tokens[_token] = true;
         emit TokenAdded(_token);
     }
 
-<<<<<<< HEAD
-=======
     /**
      * @dev Remove a cToken from the registry
      * @param _token cToken address
      */
->>>>>>> a983c67eff343846bd601e47d1453257c508091c
     function removeToken(address _token) external onlyOwner {
         require(tokens[_token] == true, "Token not exist");
         tokens[_token] = false;
         emit TokenRemoved(_token);
     }
 
-<<<<<<< HEAD
-=======
     /**
      * @dev Is token address is registered
      * @param _token cToken address
      * @return Returns `true` when cToken registered, otherwise `false`
      */
->>>>>>> a983c67eff343846bd601e47d1453257c508091c
     function isTokenExist(address _token) external view returns (bool isExist) {
         isExist = tokens[_token];
     }
