0cdbce86adb9f4f66cfb6331e6081965c3698ce5
Jitendra Chittoda <jcchittoda@gmail.com>
2020-04-19 17:55:22 +0530

    Add SavingsAccount migration script

---
+++ migrations/2_deploy_savings.js
@@ -0,0 +1,17 @@
+const SymbolsLib = artifacts.require("SymbolsLib");
+const TokenInfoLib = artifacts.require("TokenInfoLib");
+const Base = artifacts.require("Base");
+
+const SavingAccount = artifacts.require("SavingAccount");
+
+module.exports = function(deployer) {
+  deployer.deploy(SymbolsLib);
+  deployer.deploy(TokenInfoLib);
+  deployer.link(TokenInfoLib, Base);
+  deployer.deploy(Base);
+
+  deployer.link(SymbolsLib, SavingAccount);
+  deployer.link(Base, SavingAccount);
+
+  deployer.deploy(SavingAccount);
+};
