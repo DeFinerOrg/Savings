0b1cd1575cf65d9a2ec5c554034b5148eace866d
Jitendra Chittoda <jcchittoda@gmail.com>
2020-04-29 12:07:44 +0530

    Update MockCToken contract

--- contracts/mocks/MockCToken.sol
+++ contracts/mocks/MockCToken.sol
@@ -2,9 +2,42 @@ pragma solidity 0.5.11;
 
 // import { CToken } from "../Base.sol";
 
-contract MockCToken {
+import "@openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+
+contract MockCToken is Mintable {
 
     constructor() public {
 
+    }
+
+    function mint(uint mintAmount) external returns (uint) {
+        mint(msg.sender, mintAmount);
+        //TODO
+    }
+
+    function redeemUnderlying(uint redeemAmount) external returns (uint) {
+        //TODO
+    }
+
+    function redeem(uint redeemAmount) external returns (uint) {
+        burn(redeemAmount);
+        //TODO
+    }
+
+    function balanceOf(address owner) external view returns (uint256) {
+        //TODO
+    }
+
+    function supplyRatePerBlock() external view returns (uint) {
+        //TODO
+    }
+
+    function borrowRatePerBlock() external view returns (uint) {
+        //TODO
+    }
+
+    function exchangeRateStored() external view returns (uint) {
+        //TODO
     }
 }
