98a2aea4a4dd0d3c911bffa5783522b619a2eb82
Jitendra Chittoda <jcchittoda@gmail.com>
2020-05-01 07:01:39 +0530

    Test setup working

--- test/TestSavingsAccount.spec.ts
+++ test/TestSavingsAccount.spec.ts
@@ -1,100 +1,107 @@
-contract("SavingsAccount", async (accounts) => {
-  before(async () => {
-    // Things to execute before all the tests cases
-  });
+import { SavingAccountInstance, MockERC20Instance } from "../types/truffle-contracts/index";
 
-  beforeEach(async () => {
-    // Things to execute before each test cases
-  });
+const SavingAccount = artifacts.require("SavingAccount");
+const MockERC20 = artifacts.require("MockERC20");
 
-  context("constructor", async () => {
-    context("should fail", async () => {
-      it("when ...<describe the context>", async () => {
-        // Write test case here
-      });
-      it("");
+contract("SavingAccount", async (accounts) => {
+    const EMERGENCY_ADDRESS: string = "0xc04158f7dB6F9c9fFbD5593236a1a3D69F92167c";
+    const ETH_ADDRESS: string = "0x000000000000000000000000000000000000000E";
+    let savingAccount: SavingAccountInstance;
+
+    before(async () => {
+        // Things to initialize before all test
     });
 
-    context("should succeed", async () => {
-      it("when all parameters are valid");
-    });
-  });
-
-  context("depositToken()", async () => {
-    context("should fail", async () => {
-      it("when unsupported token address passed", async () => {
-        // Write the test case here
-      });
-
-      it("when amount is zero", async () => {
-        // Write the test case here
-      });
+    beforeEach(async () => {
+        // Things to execute before each test cases
+        savingAccount = await SavingAccount.new();
+        // console.log("SavingAccount: ", savingAccount.address);
     });
 
-    context("should succeed", async () => {
-      it("when supported token address is passed", async () => {
-        // Write the test case here
-      });
-    });
-  });
+    context("constructor", async () => {
+        context("should fail", async () => {
+            it("when ...<describe the context>");
+        });
 
-  context("borrow()", async () => {
-    context("should fail", async () => {
-      it("");
-    });
-
-    context("should succeed", async () => {
-      it("");
-    });
-  });
-
-  context("repay()", async () => {
-    context("should fail", async () => {
-      it("");
+        context("should succeed", async () => {
+            it("when all parameters are valid", async () => {
+                expect(await savingAccount.EMERGENCY_ADDR()).to.equal(EMERGENCY_ADDRESS);
+                expect(await savingAccount.ETH_ADDR()).equal(ETH_ADDRESS);
+            });
+        });
     });
 
-    context("should succeed", async () => {
-      it("");
-    });
-  });
+    context("depositToken()", async () => {
+        context("should fail", async () => {
+            it("when unsupported token address passed");
 
-  context("withdrawToken()", async () => {
-    context("should fail", async () => {
-      it("");
-    });
+            it("when amount is zero");
+        });
 
-    context("should succeed", async () => {
-      it("");
-    });
-  });
-
-  context("liquidate()", async () => {
-    context("should fail", async () => {
-      it("");
+        context("should succeed", async () => {
+            it("when supported token address is passed", async () => {
+                let mockERC20: MockERC20Instance = await MockERC20.new("MTK", "MockToken", 18);
+            });
+        });
     });
 
-    context("should succeed", async () => {
-      it("");
-    });
-  });
+    context("borrow()", async () => {
+        context("should fail", async () => {
+            it("");
+        });
 
-  context("toCompound()", async () => {
-    context("should fail", async () => {
-      it("");
-    });
-
-    context("should succeed", async () => {
-      it("");
-    });
-  });
-
-  context("fromCompound()", async () => {
-    context("should fail", async () => {
-      it("");
+        context("should succeed", async () => {
+            it("");
+        });
     });
 
-    context("should succeed", async () => {
-      it("");
+    context("repay()", async () => {
+        context("should fail", async () => {
+            it("");
+        });
+
+        context("should succeed", async () => {
+            it("");
+        });
     });
-  });
+
+    context("withdrawToken()", async () => {
+        context("should fail", async () => {
+            it("");
+        });
+
+        context("should succeed", async () => {
+            it("");
+        });
+    });
+
+    context("liquidate()", async () => {
+        context("should fail", async () => {
+            it("");
+        });
+
+        context("should succeed", async () => {
+            it("");
+        });
+    });
+
+    context("toCompound()", async () => {
+        context("should fail", async () => {
+            it("");
+        });
+
+        context("should succeed", async () => {
+            it("");
+        });
+    });
+
+    context("fromCompound()", async () => {
+        context("should fail", async () => {
+            it("");
+        });
+
+        context("should succeed", async () => {
+            it("");
+        });
+    });
 });
