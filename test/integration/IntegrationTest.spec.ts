b4374d93022399ad97541b7de0b4d5e65a8f49ee
Rishabh42 <rishabhthaney@gmail.com>
2020-06-02 17:03:24 +0530

    Added token instances

--- test/integration/IntegrationTest.spec.ts
+++ test/integration/IntegrationTest.spec.ts
@@ -1,13 +1,91 @@
 import * as t from "../../types/truffle-contracts/index";
 import { TestEngine } from "../../test-helpers/TestEngine";
 
-contract("Integration Tests", async (accounts) => {
-    beforeEach(async () => {});
+var chai = require("chai");
+var expect = chai.expect;
+var tokenData = require("../../test-helpers/tokenData.json");
 
-    beforeEach(async () => {});
+const { BN, expectRevert } = require("@openzeppelin/test-helpers");
+
+const MockERC20: t.MockERC20Contract = artifacts.require("MockERC20");
+const MockCToken: t.MockCTokenContract = artifacts.require("MockCToken");
+
+contract("Integration Tests", async (accounts) => {
+    const ETH_ADDRESS: string = "0x000000000000000000000000000000000000000E";
+    let testEngine: TestEngine;
+    let savingAccount: t.SavingAccountInstance;
+
+    const owner = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+    const dummy = accounts[9];
+
+    let tokens: any;
+    let addressDAI: any;
+    let addressUSDC: any;
+    let addressUSDT: any;
+    let addressTUSD: any;
+    let addressMKR: any;
+    let addressBAT: any;
+    let addressZRX: any;
+    let addressREP: any;
+    let addressWBTC: any;
+    let addressCTokenForDAI: any;
+    let addressCTokenForUSDC: any;
+    let addressCTokenForUSDT: any;
+    let addressCTokenForWBTC: any;
+    let cTokenDAI: t.MockCTokenInstance;
+    let cTokenUSDC: t.MockCTokenInstance;
+    let cTokenUSDT: t.MockCTokenInstance;
+    let cTokenWBTC: t.MockCTokenInstance;
+    let erc20DAI: t.MockERC20Instance;
+    let erc20USDC: t.MockERC20Instance;
+    let erc20USDT: t.MockERC20Instance;
+    let erc20TUSD: t.MockERC20Instance;
+    let erc20MKR: t.MockERC20Instance;
+    let erc20BAT: t.MockERC20Instance;
+    let erc20ZRX: t.MockERC20Instance;
+    let erc20REP: t.MockERC20Instance;
+    let erc20WBTC: t.MockERC20Instance;
+
+    before(async () => {
+        // Things to initialize before all test
+        testEngine = new TestEngine();
+    });
+
+    beforeEach(async () => {
+        savingAccount = await testEngine.deploySavingAccount();
+        // 1. initialization.
+        tokens = await testEngine.erc20Tokens;
+        addressDAI = tokens[0];
+        addressUSDC = tokens[1];
+        addressUSDT = tokens[2];
+        addressTUSD = tokens[3];
+        addressMKR = tokens[4];
+        addressBAT = tokens[5];
+        addressZRX = tokens[6];
+        addressREP = tokens[7];
+        addressWBTC = tokens[8];
+        erc20DAI = await MockERC20.at(addressDAI);
+        erc20USDC = await MockERC20.at(addressUSDC);
+        erc20USDT = await MockERC20.at(addressUSDT);
+        erc20TUSD = await MockERC20.at(addressTUSD);
+        erc20MKR = await MockERC20.at(addressMKR);
+        erc20WBTC = await MockERC20.at(addressWBTC);
+        addressCTokenForDAI = await testEngine.cTokenRegistry.getCToken(addressDAI);
+        addressCTokenForUSDC = await testEngine.cTokenRegistry.getCToken(addressUSDC);
+        addressCTokenForUSDT = await testEngine.cTokenRegistry.getCToken(addressUSDT);
+        addressCTokenForWBTC = await testEngine.cTokenRegistry.getCToken(addressWBTC);
+        cTokenDAI = await MockCToken.at(addressCTokenForDAI);
+        cTokenUSDC = await MockCToken.at(addressCTokenForUSDC);
+        cTokenUSDT = await MockCToken.at(addressCTokenForUSDT);
+        cTokenWBTC = await MockCToken.at(addressCTokenForWBTC);
+    });
 
     context("Deposit and Withdraw", async () => {
-        it("should deposit all tokens and withdraw all tokens");
+        it("should deposit all tokens and withdraw all tokens", async () => {
+            console.log("tt", tokens);
+        });
 
         it("should deposit all and withdraw only non-Compound tokens (MKR, TUSD)");
 
