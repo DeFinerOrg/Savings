cdec620b242879f185196f44e33882efb56b4f57
rishabh42 <rishabhthaney@gmail.com>
2020-06-23 17:45:39 +0530

    init test file for remaing savingAccount cases (from coverage)

---
+++ test/remainingCoverageTests.spec.ts
@@ -0,0 +1,47 @@
+import * as t from "../types/truffle-contracts/index";
+import { TestEngine } from "../test-helpers/TestEngine";
+
+var chai = require("chai");
+var expect = chai.expect;
+var tokenData = require("../test-helpers/tokenData.json");
+
+const { BN, expectRevert } = require("@openzeppelin/test-helpers");
+
+const SavingAccount: t.SavingAccountContract = artifacts.require("SavingAccount");
+const MockERC20: t.MockERC20Contract = artifacts.require("MockERC20");
+const MockCToken: t.MockCTokenContract = artifacts.require("MockCToken");
+const ChainLinkOracle: t.ChainLinkOracleContract = artifacts.require("ChainLinkOracle");
+
+contract("SavingAccount", async (accounts) => {
+    const EMERGENCY_ADDRESS: string = "0xc04158f7dB6F9c9fFbD5593236a1a3D69F92167c";
+    const ETH_ADDRESS: string = "0x000000000000000000000000000000000000000E";
+    const addressZero: string = "0x0000000000000000000000000000000000000000";
+    let testEngine: TestEngine;
+    let savingAccount: t.SavingAccountInstance;
+
+    const owner = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+    const dummy = accounts[9];
+
+    before(async () => {
+        // Things to initialize before all test
+        testEngine = new TestEngine();
+    });
+
+    beforeEach(async () => {
+        savingAccount = await testEngine.deploySavingAccount();
+    });
+
+    context("approveAll", async () => {
+        context("should fail", async () => {
+            it("when unsupported token address is passed");
+
+            it("when cToken address is zero");
+        });
+
+        context("should succeed", async () => {
+            it("when all conditions are satisfied");
+        });
+    });
+});
