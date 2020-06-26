67a9b3cdaab27f43d75f820c09db03ea3e454fe3
rishabh42 <rishabhthaney@gmail.com>
2020-06-26 15:31:52 +0530

    mockAggregator rename, expect change for test

--- test/remainingCoverageTests.spec.ts
+++ test/remainingCoverageTests.spec.ts
@@ -118,8 +118,10 @@ contract("SavingAccount", async (accounts) => {
                 // 3. Verify the loan amount
                 const user2Balance = await erc20DAI.balanceOf(user2);
 
-                let mockChainlinkAggregatorforDAIAddress: string = testEngine.aggregators[0];
-                let mockChainlinkAggregatorforUSDCAddress: string = testEngine.aggregators[1];
+                let mockChainlinkAggregatorforDAIAddress: string =
+                    testEngine.mockChainlinkAggregators[0];
+                let mockChainlinkAggregatorforUSDCAddress: string =
+                    testEngine.mockChainlinkAggregators[1];
 
                 const mockChainlinkAggregatorforDAI: t.MockChainLinkAggregatorInstance = await MockChainLinkAggregator.at(
                     mockChainlinkAggregatorforDAIAddress
@@ -141,7 +143,7 @@ contract("SavingAccount", async (accounts) => {
                     user2,
                     addressDAI
                 );
-                //expect(isAccountLiquidatableStr).equal(false);
+                expect(isAccountLiquidatableStr).equal(true);
                 // should return "True"
             });
 
