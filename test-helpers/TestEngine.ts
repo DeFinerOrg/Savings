bad9d7c11464546329c2bd4f532d0b568e06d878
王广宇 <382303784@qq.com>
2020-06-29 11:04:51 +0800

    fixed TestEngine.ts

--- test-helpers/TestEngine.ts
+++ test-helpers/TestEngine.ts
@@ -21,7 +21,6 @@ export class TestEngine {
     public async deployMockCTokens(erc20Tokens: Array<string>): Promise<Array<string>> {
         const network = process.env.NETWORK;
         var cTokens = new Array();
-
         await Promise.all(
             erc20Tokens.map(async (tokenAddr: any) => {
                 let addr;
@@ -36,7 +35,8 @@ export class TestEngine {
                 cTokens.push(addr);
             })
         );
-
+        let addr = (await MockCToken.new(ETH_ADDR)).address;
+        cTokens.push(addr)
         return cTokens;
     }
 
@@ -60,7 +60,6 @@ export class TestEngine {
                 erc20TokenAddresses.push(addr);
             })
         );
-
         return erc20TokenAddresses;
     }
 
@@ -85,7 +84,13 @@ export class TestEngine {
                 this.mockChainlinkAggregators.push(addr);
             })
         );
-
+        let addr = (
+            await MockChainLinkAggregator.new(
+                tokenData.ETH.decimals,
+                new BN(tokenData.ETH.latestAnswer)
+            )
+        ).address;
+        this.mockChainlinkAggregators.push(addr);
         return this.mockChainlinkAggregators;
     }
 
@@ -131,6 +136,13 @@ export class TestEngine {
                 );
             })
         );
-        //await tokenInfoRegistry.addToken(ETH_ADDR);
+        await this.tokenInfoRegistry.addToken(
+            ETH_ADDR,
+            18,
+            false,
+            true,
+            cTokens[9],
+            aggregators[9]
+        );
     }
 }
