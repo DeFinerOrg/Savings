a7a083d26873fdcedd0a39227bd82f3d77e4e3c7
Guangyu Wang <382303784@qq.com>
2020-07-28 10:28:57 +0800

    withdraw test

--- test/unit/TestChainLinkOracle.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as t from "../../types/truffle-contracts/index";
-
-contract("ChainLinkOracle", async (accounts) => {
-    context("constructor", async () => {
-        it("should initialize");
-    });
-});
