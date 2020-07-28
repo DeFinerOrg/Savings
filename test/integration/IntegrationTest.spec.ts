1e8caad05c10ac62b1b4921cf8fab25c21f678e3
Guangyu Wang <382303784@qq.com>
2020-07-28 11:30:37 +0800

    update getPrice

--- test/integration/IntegrationTest.spec.ts
+++ test/integration/IntegrationTest.spec.ts
@@ -190,9 +190,10 @@ contract("Integration Tests", async (accounts) => {
                     console.log(addressCTokenTemp);
                     cTokenTemp = await MockCToken.at(addressCTokenTemp);
 
-                    console.log("1");
                     let price = await savingAccount.getPrice(tempContractAddress);
                     console.log(price[0].toString(), price[1].toString());
+
+                    console.log("1");
                     await savingAccount.withdrawAll(erc20contr.address, {
                         from: user1,
                     });
