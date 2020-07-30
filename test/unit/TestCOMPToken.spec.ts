2653e47113344a2f1e527cfe2d37f25c4b1a88fd
Guangyu Wang <382303784@qq.com>
2020-07-30 17:32:36 +0800

    fixed bug

--- test/unit/TestCOMPToken.spec.ts
+++ test/unit/TestCOMPToken.spec.ts
@@ -13,7 +13,7 @@ const MockCToken: t.MockCTokenContract = artifacts.require("MockCToken");
 const ChainLinkOracle: t.ChainLinkOracleContract = artifacts.require("ChainLinkOracle");
 const GlobalConfig: t.GlobalConfigContract = artifacts.require("GlobalConfig");
 
-contract("GlobalConfig", async (accounts) => {
+contract("SavingAccount.COMP", async (accounts) => {
     const EMERGENCY_ADDRESS: string = "0xc04158f7dB6F9c9fFbD5593236a1a3D69F92167c";
     const ETH_ADDRESS: string = "0x000000000000000000000000000000000000000E";
     const addressZero: string = "0x0000000000000000000000000000000000000000";
@@ -27,6 +27,24 @@ contract("GlobalConfig", async (accounts) => {
     const user2 = accounts[2];
     const dummy = accounts[9];
 
+    const eighteenPrecision = new BN(10).pow(new BN(18));
+    const sixPrecision = new BN(10).pow(new BN(6));
+    const eightPrecision = new BN(10).pow(new BN(8));
+
+    let tokens: any;
+    let addressDAI: any;
+    let addressUSDC: any;
+    let addressTUSD: any;
+    let addressMKR: any;
+    let addressCTokenForDAI: any;
+    let addressCTokenForUSDC: any;
+    let cDAI: t.MockCTokenInstance;
+    let cUSDC: t.MockCTokenInstance;
+    let erc20DAI: t.ERC20Instance;
+    let erc20USDC: t.ERC20Instance;
+    let erc20TUSD: t.ERC20Instance;
+    let erc20MKR: t.ERC20Instance;
+
     before(async () => {
         // Things to initialize before all test
         testEngine = new TestEngine();
@@ -37,6 +55,23 @@ contract("GlobalConfig", async (accounts) => {
         savingAccount = await testEngine.deploySavingAccount();
         globalConfig = await testEngine.globalConfig;
         COMPTokenAddress = await testEngine.getCOMPTokenAddress();
+
+        // 1. initialization.
+        tokens = await testEngine.erc20Tokens;
+        addressDAI = tokens[0];
+        addressUSDC = tokens[1];
+        addressTUSD = tokens[3];
+        addressMKR = tokens[4];
+        // Use ERC20 from OZ, import this
+        erc20DAI = await ERC20.at(addressDAI);
+        erc20USDC = await ERC20.at(addressUSDC);
+        erc20TUSD = await ERC20.at(addressTUSD);
+        erc20MKR = await ERC20.at(addressMKR);
+        addressCTokenForDAI = await testEngine.tokenInfoRegistry.getCToken(addressDAI);
+        addressCTokenForUSDC = await testEngine.tokenInfoRegistry.getCToken(addressUSDC);
+        // Use CERC20, import from Compound
+        cDAI = await MockCToken.at(addressCTokenForDAI);
+        cUSDC = await MockCToken.at(addressCTokenForUSDC);
     });
 
     context("constructor", async () => {
@@ -47,115 +82,27 @@ contract("GlobalConfig", async (accounts) => {
                     "Community fund is zero"
                 );
             });
-
-            it("When executing updateMinReserveRatio, the input parameter is zero.", async () => {
-                await expectRevert(
-                    globalConfig.updateMinReserveRatio(new BN(0)),
-                    "Min Reserve Ratio is zero"
-                );
-            });
-
-            it("When executing updateMinReserveRatio, the input parameter is greater than maxReserveRatio.", async () => {
-                await expectRevert(
-                    globalConfig.updateMinReserveRatio(new BN(21)),
-                    "Min reserve greater or equal to Max reserve"
-                );
-            });
-
-            it("When executing updateMaxReserveRatio, the input parameter is zero.", async () => {
-                await expectRevert(
-                    globalConfig.updateMaxReserveRatio(new BN(0)),
-                    "Max Reserve Ratio is zero"
-                );
-            });
-
-            it("When executing updateMaxReserveRatio, the input parameter is less than minReserveRatio.", async () => {
-                await expectRevert(
-                    globalConfig.updateMaxReserveRatio(new BN(9)),
-                    "Max reserve less than or equal to Min reserve"
-                );
-            });
-
-            it("When executing updateLiquidationThreshold, the input parameter is zero.", async () => {
-                await expectRevert(
-                    globalConfig.updateLiquidationThreshold(new BN(0)),
-                    "LiquidationThreshold is zero"
-                );
-            });
-
-            it("When executing updateLiquidationDiscountRatio, the input parameter is zero.", async () => {
-                await expectRevert(
-                    globalConfig.updateLiquidationDiscountRatio(new BN(0)),
-                    "LiquidationDiscountRatio is zero"
-                );
-            });
         });
 
         context("should succeed", async () => {
-            it("executing updateCommunityFundRatio", async () => {
+            it("Withdraw COMP token", async () => {
+                //1. set deFinerCommunityFund address.
+                await globalConfig.updateDeFinerCommunityFund(owner);
+                //2. set COMPToken address.
+                await globalConfig.updateCompoundAddress(COMPTokenAddress);
+                //3. Deposit token.
+                const ONE_DAI = eighteenPrecision.mul(new BN(1));
+                await erc20DAI.approve(savingAccount.address, numOfToken);
+                //4. After a while.
+
+                //5. Withdraw COMP token.
+
+                //6. Compare the results..
                 const beforeCommunityFundRatio = await globalConfig.communityFundRatio();
                 await globalConfig.updateCommunityFundRatio(new BN(20));
                 const afterCommunityFundRatio = await globalConfig.communityFundRatio();
                 expect(beforeCommunityFundRatio).to.be.bignumber.equal(new BN(10));
                 expect(afterCommunityFundRatio).to.be.bignumber.equal(new BN(20));
-            });
-
-            it("executing updateMinReserveRatio", async () => {
-                const beforeMinReserveRatio = await globalConfig.minReserveRatio();
-                await globalConfig.updateMinReserveRatio(new BN(15));
-                const afterMinReserveRatio = await globalConfig.minReserveRatio();
-                expect(beforeMinReserveRatio).to.be.bignumber.equal(new BN(10));
-                expect(afterMinReserveRatio).to.be.bignumber.equal(new BN(15));
-            });
-
-            it("executing updateMaxReserveRatio", async () => {
-                const beforeMaxReserveRatio = await globalConfig.maxReserveRatio();
-                await globalConfig.updateMaxReserveRatio(new BN(25));
-                const afterMaxReserveRatio = await globalConfig.maxReserveRatio();
-                expect(beforeMaxReserveRatio).to.be.bignumber.equal(new BN(20));
-                expect(afterMaxReserveRatio).to.be.bignumber.equal(new BN(25));
-            });
-
-            it("executing updateLiquidationThreshold", async () => {
-                const beforeLiquidationThreshold = await globalConfig.liquidationThreshold();
-                await globalConfig.updateLiquidationThreshold(new BN(20));
-                const afterLiquidationThreshold = await globalConfig.liquidationThreshold();
-                expect(beforeLiquidationThreshold).to.be.bignumber.equal(new BN(85));
-                expect(afterLiquidationThreshold).to.be.bignumber.equal(new BN(20));
-            });
-
-            it("executing updateLiquidationDiscountRatio", async () => {
-                const beforeLiquidationDiscountRatio = await globalConfig.liquidationDiscountRatio();
-                await globalConfig.updateLiquidationDiscountRatio(new BN(20));
-                const afterLiquidationDiscountRatio = await globalConfig.liquidationDiscountRatio();
-                expect(beforeLiquidationDiscountRatio).to.be.bignumber.equal(new BN(95));
-                expect(afterLiquidationDiscountRatio).to.be.bignumber.equal(new BN(20));
-            });
-
-            it("executing midReserveRatio", async () => {
-                const beforeLiquidationDiscountRatio = await globalConfig.midReserveRatio();
-                await globalConfig.updateMinReserveRatio(new BN(15));
-                await globalConfig.updateMaxReserveRatio(new BN(25));
-                const afterLiquidationDiscountRatio = await globalConfig.midReserveRatio();
-                expect(beforeLiquidationDiscountRatio).to.be.bignumber.equal(new BN(15));
-                expect(afterLiquidationDiscountRatio).to.be.bignumber.equal(new BN(20));
-            });
-
-            it("executing updateDeFinerCommunityFund", async () => {
-                const beforeDeFinerCommunityFund = await globalConfig.deFinerCommunityFund();
-                await globalConfig.updateDeFinerCommunityFund(owner);
-                const afterDeFinerCommunityFund = await globalConfig.deFinerCommunityFund();
-                expect(beforeDeFinerCommunityFund).to.equal(addressZero);
-                expect(afterDeFinerCommunityFund).to.equal(owner);
-            });
-
-            it("executing updateCompoundAddress", async () => {
-                const beforeCompoundAddress = await globalConfig.compoundAddress();
-                console.log(COMPTokenAddress);
-                await globalConfig.updateMinReserveRatio(COMPTokenAddress);
-                const afterCompoundAddress = await globalConfig.compoundAddress();
-                expect(beforeCompoundAddress).to.equal(addressZero);
-                expect(afterCompoundAddress).to.equal(COMPTokenAddress);
             });
         });
     });
