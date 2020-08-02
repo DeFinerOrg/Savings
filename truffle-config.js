43b07c7de5263f5cabb361fe9e94985b4a3626d4
Guangyu Wang <382303784@qq.com>
2020-08-03 01:10:37 +0800

    fixed network_id

--- truffle-config.js
+++ truffle-config.js
@@ -91,7 +91,7 @@ module.exports = {
                     "https://kovan.infura.io/v3/cf38c21326954ac28aa4f8c3ee33550c"
                 ),
             from: "0xe6A7bc2c96e4374eBCdB23aEDCBB6Ef1eB3d4C83", // default address to use for any transaction Truffle makes during migrations
-            network_id: 3,
+            network_id: 42,
             gas: 6000000,
             gasPrice: 15000000000,
         }
