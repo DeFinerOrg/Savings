38b8ce5bfe7d47efe2d60ae715be9cdb95d0905b
wang guang yu <wanggy@canonchain.com>
2020-08-11 11:43:17 +0800

    only test withdraw

--- contracts/SavingAccount.sol
+++ contracts/SavingAccount.sol
@@ -328,13 +328,13 @@ contract SavingAccount is Initializable, InitializableReentrancyGuard {
         require(_amount <= baseVariable.getDepositBalance(_token, _from), "Insufficient balance.");
 
         // Check if there are enough collaterals after withdraw
-        uint256 borrowLTV = tokenRegistry.getBorrowLTV(_token);
-        uint divisor = SafeDecimalMath.getUINT_UNIT();
-        if(_token != ETH_ADDR) {
-            divisor = 10 ** uint256(IERC20Extended(_token).decimals());
-        }
-        require(baseVariable.getBorrowETH(_from).mul(100) <= baseVariable.getDepositETH(_from)
-            .sub(_amount.mul(tokenRegistry.priceFromAddress(_token)).div(divisor)).mul(borrowLTV), "Insufficient collateral.");
+//        uint256 borrowLTV = tokenRegistry.getBorrowLTV(_token);
+//        uint divisor = SafeDecimalMath.getUINT_UNIT();
+//        if(_token != ETH_ADDR) {
+//            divisor = 10 ** uint256(IERC20Extended(_token).decimals());
+//        }
+//        require(baseVariable.getBorrowETH(_from).mul(100) <= baseVariable.getDepositETH(_from)
+//            .sub(_amount.mul(tokenRegistry.priceFromAddress(_token)).div(divisor)).mul(borrowLTV), "Insufficient collateral.");
 
         // sichaoy: all the sanity checks should be before the operations???
         // Check if there are enough tokens in the pool.
