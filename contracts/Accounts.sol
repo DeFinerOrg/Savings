d021b5cdd5337e5a050b2c525b82ff2c59620be1
wang guang yu <wanggy@canonchain.com>
2020-08-12 15:07:27 +0800

    add Accounts.sol

---
+++ contracts/Accounts.sol
@@ -0,0 +1,179 @@
+pragma solidity 0.5.14;
+
+import "./lib/TokenInfoLib.sol";
+import "./lib/BitmapLib.sol";
+
+contract Accounts {
+    using TokenInfoLib for TokenInfoLib.TokenInfo;
+    using BitmapLib for uint128;
+
+    mapping(address => Account) accounts;
+
+    struct Account {
+        // Note, it's best practice to use functions minusAmount, addAmount, totalAmount
+        // to operate tokenInfos instead of changing it directly.
+        mapping(address => TokenInfoLib.TokenInfo) tokenInfos;
+        uint128 depositBitmap;
+        uint128 borrowBitmap;
+    }
+
+    /**
+     * Check if the user has deposit for any tokens
+     * @param _account address of the user
+     * @return true if the user has positive deposit balance
+     */
+    function isUserHasAnyDeposits(address _account) public view returns (bool) {
+        Account storage account = accounts[_account];
+        return account.depositBitmap > 0;
+    }
+
+    /**
+     * Check if the user has deposit for a token
+     * @param _account address of the user
+     * @param _index index of the token
+     * @return true if the user has positive deposit balance for the token
+     */
+    function isUserHasDeposits(address _account, uint8 _index) public view returns (bool) {
+        Account storage account = accounts[_account];
+        return account.depositBitmap.isBitSet(_index);
+    }
+
+    /**
+     * Check if the user has borrowed a token
+     * @param _account address of the user
+     * @param _index index of the token
+     * @return true if the user has borrowed the token
+     */
+    function isUserHasBorrows(address _account, uint8 _index) public view returns (bool) {
+        Account storage account = accounts[_account];
+        return account.borrowBitmap.isBitSet(_index);
+    }
+
+    /**
+     * Set the deposit bitmap for a token.
+     * @param _account address of the user
+     * @param _index index of the token
+     */
+    function setInDepositBitmap(address _account, uint8 _index) public {
+        Account storage account = accounts[_account];
+        account.depositBitmap = account.depositBitmap.setBit(_index);
+    }
+
+    /**
+     * Unset the deposit bitmap for a token
+     * @param _account address of the user
+     * @param _index index of the token
+     */
+    function unsetFromDepositBitmap(address _account, uint8 _index) public {
+        Account storage account = accounts[_account];
+        account.depositBitmap = account.depositBitmap.unsetBit(_index);
+    }
+
+    /**
+     * Set the borrow bitmap for a token.
+     * @param _account address of the user
+     * @param _index index of the token
+     */
+    function setInBorrowBitmap(address _account, uint8 _index) public {
+        Account storage account = accounts[_account];
+        account.borrowBitmap = account.borrowBitmap.setBit(_index);
+    }
+
+    /**
+     * Unset the borrow bitmap for a token
+     * @param _account address of the user
+     * @param _index index of the token
+     */
+    function unsetFromBorrowBitmap(address _account, uint8 _index) public {
+        Account storage account = accounts[_account];
+        account.borrowBitmap = account.borrowBitmap.unsetBit(_index);
+    }
+
+    function getDepositBalance(
+        address _token,
+        address _accountAddr
+    ) public view returns (uint256 depositBalance) {
+        TokenInfoLib.TokenInfo storage tokenInfo = accounts[_accountAddr].tokenInfos[_token];
+        uint UNIT = SafeDecimalMath.getUNIT();
+        uint accruedRate;
+        if(tokenInfo.getDepositPrincipal() == 0) {
+            return 0;
+        } else {
+            if(depositeRateIndex[_token][tokenInfo.getLastDepositBlock()] == 0) {
+                accruedRate = UNIT;
+            } else {
+                accruedRate = depositRateIndexNow(_token)
+                .mul(UNIT)
+                .div(depositeRateIndex[_token][tokenInfo.getLastDepositBlock()]);
+            }
+            return tokenInfo.getDepositBalance(accruedRate);
+        }
+    }
+
+    /**
+     * Get current borrow balance of a token
+     * @param _token token address
+     * @dev This is an estimation. Add a new checkpoint first, if you want to derive the exact balance.
+     */
+    // sichaoy: What's the diff of getBorrowBalance with getBorrowAcruedRate?
+    function getBorrowBalance(
+        address _token,
+        address _accountAddr
+    ) public view returns (uint256 borrowBalance) {
+        TokenInfoLib.TokenInfo storage tokenInfo = accounts[_accountAddr].tokenInfos[_token];
+        uint UNIT = SafeDecimalMath.getUNIT();
+        uint accruedRate;
+        if(tokenInfo.getBorrowPrincipal() == 0) {
+            return 0;
+        } else {
+            if(borrowRateIndex[_token][tokenInfo.getLastBorrowBlock()] == 0) {
+                accruedRate = UNIT;
+            } else {
+                accruedRate = borrowRateIndexNow(_token)
+                .mul(UNIT)
+                .div(borrowRateIndex[_token][tokenInfo.getLastBorrowBlock()]);
+            }
+            return tokenInfo.getBorrowBalance(accruedRate);
+        }
+    }
+
+    /**
+     * Get current deposit balance of a token
+     * @dev This is an estimation. Add a new checkpoint first, if you want to derive the exact balance.
+     */
+    function getDepositETH(
+        address _accountAddr
+    ) public view returns (uint256 depositETH) {
+        for(uint i = 0; i < TokenInfoRegistry(tokenInfoRegistryAddress).getCoinLength(); i++) {
+            if(isUserHasDeposits(_accountAddr, uint8(i))) {
+                address tokenAddress = TokenInfoRegistry(tokenInfoRegistryAddress).addressFromIndex(i);
+                uint divisor = INT_UNIT;
+                if(tokenAddress != ETH_ADDR) {
+                    divisor = 10**uint256(TokenInfoRegistry(tokenInfoRegistryAddress).getTokenDecimals(tokenAddress));
+                }
+                depositETH = depositETH.add(getDepositBalance(tokenAddress, _accountAddr).mul(TokenInfoRegistry(tokenInfoRegistryAddress).priceFromIndex(i)).div(divisor));
+            }
+        }
+        return depositETH;
+    }
+
+    /**
+     * Get borrowed balance of a token in the uint of Wei
+     */
+    // sichaoy: change name to getTotalBorrowInETH()
+    function getBorrowETH(
+        address _accountAddr
+    ) public view returns (uint256 borrowETH) {
+        for(uint i = 0; i < TokenInfoRegistry(tokenInfoRegistryAddress).getCoinLength(); i++) {
+            if(isUserHasBorrows(_accountAddr, uint8(i))) {
+                address tokenAddress = TokenInfoRegistry(tokenInfoRegistryAddress).addressFromIndex(i);
+                uint divisor = INT_UNIT;
+                if(tokenAddress != ETH_ADDR) {
+                    divisor = 10**uint256(TokenInfoRegistry(tokenInfoRegistryAddress).getTokenDecimals(tokenAddress));
+                }
+                borrowETH = borrowETH.add(getBorrowBalance(tokenAddress, _accountAddr).mul(TokenInfoRegistry(tokenInfoRegistryAddress).priceFromIndex(i)).div(divisor));
+            }
+        }
+        return borrowETH;
+    }
+}
