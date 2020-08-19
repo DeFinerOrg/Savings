2a4cde0d147c86ed7cd30fcb7b36e83e83626d59
sichaoy <36025075+sichaoy@users.noreply.github.com>
2020-08-18 23:28:25 -0400

    Error fix for audit (#123)
    * Update the expected error message in a test case.
    
    * Fixed a few errors in test code mainly due to the name change in source code.
    
    Co-authored-by: Sichao Yang <ysc@canonchain.com>

--- test/unit/liquidateTests.spec.ts
+++ test/unit/liquidateTests.spec.ts
@@ -20,7 +20,7 @@ contract("SavingAccount.liquidate", async (accounts) => {
     const addressZero: string = "0x0000000000000000000000000000000000000000";
     let testEngine: TestEngine;
     let savingAccount: t.SavingAccountWithControllerInstance;
-    let tokenInfoRegistry: t.TokenInfoRegistryInstance;
+    let tokenInfoRegistry: t.TokenRegistryInstance;
     let accountsContract: t.AccountsInstance;
 
     const owner = accounts[0];
