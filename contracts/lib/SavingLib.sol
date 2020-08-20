6d29380a80611662347a82626111e90fad5eff7f
wanggy <382303784@qq.com>
2020-08-20 22:43:10 +0800

    update SavingLib.sol

--- contracts/lib/SavingLib.sol
+++ contracts/lib/SavingLib.sol
@@ -62,4 +62,30 @@ library SavingLib {
         }
     }
 
+    /**
+     * Deposit the amount of token to the saving pool.
+     * @param _to the account that the token deposit to.
+     * @param _token the address of the deposited token
+     * @param _amount the amount of the deposited token
+     */
+     // sichaoy: should not be public, why cannot we find _tokenIndex from token address?
+    function deposit(GlobalConfig globalConfig, address _to, address _token, uint256 _amount) public {
+
+        require(_amount != 0, "Amount is zero");
+
+        // Add a new checkpoint on the index curve.
+        globalConfig.bank().newRateIndexCheckpoint(_token);
+
+        // Update tokenInfo. Add the _amount to principal, and update the last deposit block in tokenInfo
+        globalConfig.accounts().deposit(_to, _token, _amount, address(this).getBlockNumber());
+
+        // Update the amount of tokens in compound and loans, i.e. derive the new values
+        // of C (Compound Ratio) and U (Utilization Ratio).
+//        globalConfig.bank().updateTotalCompound(_token);
+//        globalConfig.bank().updateTotalLoan(_token);
+//        uint compoundAmount = globalConfig.bank().updateTotalReserve(_token, _amount, globalConfig.bank().Deposit()); // Last parameter false means deposit token
+        uint compoundAmount = globalConfig.bank().update(_token, _amount, globalConfig.bank().Deposit());
+        SavingLib.toCompound(globalConfig, _token, compoundAmount);
+    }
+
 }
