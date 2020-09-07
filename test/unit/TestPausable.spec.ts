722970bd45f6087801021be5ad27b46e38595255
wanggy <382303784@qq.com>
2020-09-08 00:41:36 +0800

    add TestPausable

---
+++ test/unit/TestPausable.spec.ts
@@ -0,0 +1,100 @@
+import * as t from "../../types/truffle-contracts/index";
+import { TestEngine } from "../../test-helpers/TestEngine";
+
+var chai = require("chai");
+var expect = chai.expect;
+var tokenData = require("../../test-helpers/tokenData.json");
+
+const { BN, expectRevert } = require("@openzeppelin/test-helpers");
+
+const SavingAccount: t.SavingAccountContract = artifacts.require("SavingAccount");
+const ERC20: t.Erc20Contract = artifacts.require("ERC20");
+const MockCToken: t.MockCTokenContract = artifacts.require("MockCToken");
+const ChainLinkAggregator: t.ChainLinkAggregatorContract = artifacts.require("ChainLinkAggregator");
+
+contract("InitializablePausable", async (accounts) => {
+    const EMERGENCY_ADDRESS: string = "0xc04158f7dB6F9c9fFbD5593236a1a3D69F92167c";
+    const ETH_ADDRESS: string = "0x000000000000000000000000000000000000000E";
+    const addressZero: string = "0x0000000000000000000000000000000000000000";
+    let testEngine: TestEngine;
+    let savingAccount: t.SavingAccountWithControllerInstance;
+
+    const owner = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+    const dummy = accounts[9];
+    // testEngine = new TestEngine();
+    // testEngine.deploy("scriptFlywheel.scen");
+
+    before(function () {
+        // Things to initialize before all test  
+        this.timeout(0);
+        testEngine = new TestEngine();
+        testEngine.deploy("scriptFlywheel.scen");
+    });
+
+    beforeEach(async () => {
+        savingAccount = await testEngine.deploySavingAccount();
+    });
+
+    context("constructor", async () => {
+        context("should fail", async () => {
+            it("The non-owner calls the function that can be suspended.", async () => {
+                await expectRevert(
+                    savingAccount.pause({from: user1}),
+                    "Ownable: caller is not the owner"
+                );
+            });
+        });
+
+        context("should succeed", async () => {
+            it("The test turns on the pause function.", async () => {
+
+                const depositAmount = new BN(100);
+
+                const beforePaused = await savingAccount.paused();
+
+                await savingAccount.pause();
+
+                await expectRevert(
+                    savingAccount.deposit(ETH_ADDRESS, depositAmount, {
+                        value: depositAmount
+                    }),
+                    "Pausable: paused"
+                );
+                const afterPaused = await savingAccount.paused();
+
+                expect(beforePaused).to.equal(false);
+                expect(afterPaused).to.equal(true);
+            });
+
+            it("The test turns off the pause function.", async () => {
+                const depositAmount = new BN(100);
+                const beforePaused = await savingAccount.paused();
+
+                await savingAccount.pause();
+
+                const midPaused = await savingAccount.paused();
+
+                await savingAccount.unpause();
+                await expectRevert(
+                    savingAccount.deposit(ETH_ADDRESS, depositAmount, {
+                        value: depositAmount
+                    }),
+                    "Pausable: paused"
+                );
+
+                const afterPaused = await savingAccount.paused();
+
+                await savingAccount.deposit(ETH_ADDRESS, depositAmount, {
+                    value: depositAmount
+                })
+
+                expect(beforePaused).to.equal(false);
+                expect(midPaused).to.equal(true);
+                expect(afterPaused).to.equal(false);
+            });
+        });
+    });
+
+});
