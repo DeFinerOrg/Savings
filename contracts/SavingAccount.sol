3e89e050e705507df33d3b59416f46d195346a9c
Rishabh Thaney <rishabhthaney@gmail.com>
2020-09-24 13:59:54 +0530

    fixed 'unused return' vulnerability

--- contracts/SavingAccount.sol
+++ contracts/SavingAccount.sol
@@ -322,7 +322,7 @@ contract SavingAccount is Initializable, InitializableReentrancyGuard, Constant,
         // of loan in SavingAccount should be updated though the reservation and compound parts will not changed.
         uint256 targetTokenTransfer = totalBorrowBeforeLiquidation.sub(vars.totalBorrow).mul(divisor).div(vars.targetTokenPrice);
         uint256 amount = globalConfig.bank().withdraw(msg.sender, _targetToken, targetTokenTransfer);
-        globalConfig.bank().repay(_targetAccountAddr, _targetToken, amount);
+        uint256 repayAmount = globalConfig.bank().repay(_targetAccountAddr, _targetToken, amount);
     }
 
     /**
