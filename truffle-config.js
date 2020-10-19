02fcebd501230a5e9f3a06cb6923001daeaae8b1
Rishabh Thaney <rishabh@hh-eth-vm3.3rjy1fkcmmoubkgxgo2xvp1iig.bx.internal.cloudapp.net>
2020-10-19 10:41:49 +0000

    increased gas limit

--- truffle-config.js
+++ truffle-config.js
@@ -60,7 +60,7 @@ module.exports = {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*",
-            gas: 8000000,
+            gas: 20000000,
         },
         coverage: {
             host: "127.0.0.1",
