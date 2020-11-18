2be70ea77163eabe7c0cdbca6b21019bf1250d0e
Rishabh Thaney <rishabhthaney@gmail.com>
2020-11-18 19:00:53 +0530

    fixed TODOs

--- test/long_duration/borrowRepayTestDAI.spec.ts
+++ test/long_duration/borrowRepayTestDAI.spec.ts
@@ -283,7 +283,6 @@ contract("SavingAccount.borrowRepayTestDAI", async (accounts) => {
 
                     // Second, verify the interest rate calculation. Need to compare these value to
                     // the rate simulator.
-                    // TODO:
                     expect(BN(totalDepositInterest)).to.be.bignumber.equal(new BN(1503650800000)); // 3007210014379.6274/2 || 1503559214300
                     expect(BN(totalBorrowInterest)).to.be.bignumber.equal(new BN(0));
                     expect(BN(totalCompoundInterest)).to.be.bignumber.equal(new BN(9585493199));
