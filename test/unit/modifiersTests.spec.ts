db2f5096d4942faa9c3effd1885b2ebd4180675d
Rishabh <rishabhthaney@gmail.com>
2021-01-08 20:28:22 +0530

    onlyOwner tests

---
+++ test/unit/modifiersTests.spec.ts
@@ -0,0 +1,182 @@
+import { TokenRegistryContract } from "./../../types/truffle-contracts/index.d";
+import { BigNumber } from "bignumber.js";
+import * as t from "../../types/truffle-contracts/index";
+import { TestEngine } from "../../test-helpers/TestEngine";
+import { savAccBalVerify } from "../../test-helpers/lib/lib";
+
+var chai = require("chai");
+var expect = chai.expect;
+var tokenData = require("../../test-helpers/tokenData.json");
+const MockChainLinkAggregator: t.MockChainLinkAggregatorContract = artifacts.require(
+    "MockChainLinkAggregator"
+);
+const { BN, expectRevert, time } = require("@openzeppelin/test-helpers");
+
+const ERC20: t.MockErc20Contract = artifacts.require("MockERC20");
+const MockCToken: t.MockCTokenContract = artifacts.require("MockCToken");
+const GlobalConfig: t.GlobalConfigContract = artifacts.require("GlobalConfig");
+
+contract("SavingAccount.withdraw", async (accounts) => {
+    const ETH_ADDRESS: string = "0x000000000000000000000000000000000000000E";
+    const addressZero: string = "0x0000000000000000000000000000000000000000";
+    let testEngine: TestEngine;
+    let savingAccount: t.SavingAccountWithControllerInstance;
+    let accountsContract: t.AccountsInstance;
+    let bank: t.BankInstance;
+    let globalConfig: t.GlobalConfigInstance;
+    let tokenInfoRegistry: t.TokenRegistryInstance;
+
+    const owner = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+    const user3 = accounts[3];
+    const dummy = accounts[9];
+    const eighteenPrecision = new BN(10).pow(new BN(18));
+    const sixPrecision = new BN(10).pow(new BN(6));
+    const eightPrecision = new BN(10).pow(new BN(8));
+    let tokens: any;
+    let mockChainlinkAggregators: any;
+    let addressDAI: any;
+    let addressUSDC: any;
+    let addressUSDT: any;
+    let addressTUSD: any;
+    let addressMKR: any;
+    let addressWBTC: any;
+    let mockChainlinkAggregatorforDAIAddress: any;
+    let mockChainlinkAggregatorforUSDCAddress: any;
+    let mockChainlinkAggregatorforUSDTAddress: any;
+    let mockChainlinkAggregatorforTUSDAddress: any;
+    let mockChainlinkAggregatorforMKRAddress: any;
+    let mockChainlinkAggregatorforWBTCAddress: any;
+    let mockChainlinkAggregatorforETHAddress: any;
+    let cDAI_addr: any;
+    let cUSDC_addr: any;
+    let cUSDT_addr: any;
+    let cWBTC_addr: any;
+    let cETH_addr: any;
+    let cETH: any;
+
+    let cDAI: t.MockCTokenInstance;
+    let cUSDC: t.MockCTokenInstance;
+    let cUSDT: t.MockCTokenInstance;
+    let cWBTC: t.MockCTokenInstance;
+
+    let erc20DAI: t.MockErc20Instance;
+    let erc20USDC: t.MockErc20Instance;
+    let erc20MKR: t.MockErc20Instance;
+    let erc20TUSD: t.MockErc20Instance;
+    let erc20USDT: t.MockErc20Instance;
+    let erc20WBTC: t.MockErc20Instance;
+    let mockChainlinkAggregatorforDAI: t.MockChainLinkAggregatorInstance;
+    let mockChainlinkAggregatorforUSDC: t.MockChainLinkAggregatorInstance;
+    let mockChainlinkAggregatorforUSDT: t.MockChainLinkAggregatorInstance;
+    let mockChainlinkAggregatorforTUSD: t.MockChainLinkAggregatorInstance;
+    let mockChainlinkAggregatorforWBTC: t.MockChainLinkAggregatorInstance;
+    let mockChainlinkAggregatorforMKR: t.MockChainLinkAggregatorInstance;
+    let mockChainlinkAggregatorforETH: t.MockChainLinkAggregatorInstance;
+    let ONE_DAI: any;
+    let ONE_USDC: any;
+    let ZERO: any;
+    // testEngine = new TestEngine();
+    // testEngine.deploy("scriptFlywheel.scen");
+
+    before(function () {
+        // Things to initialize before all test
+        this.timeout(0);
+        testEngine = new TestEngine();
+        testEngine.deploy("scriptFlywheel.scen");
+    });
+
+    beforeEach(async function () {
+        this.timeout(0);
+        savingAccount = await testEngine.deploySavingAccount();
+        accountsContract = await testEngine.accounts;
+        bank = await testEngine.bank;
+        globalConfig = await testEngine.globalConfig;
+        tokenInfoRegistry = await testEngine.tokenInfoRegistry;
+        // initialization.
+        tokens = await testEngine.erc20Tokens;
+        mockChainlinkAggregators = await testEngine.mockChainlinkAggregators;
+        addressDAI = tokens[0];
+        addressUSDC = tokens[1];
+        addressUSDT = tokens[2];
+        addressTUSD = tokens[3];
+        addressMKR = tokens[4];
+        addressWBTC = tokens[8];
+
+        mockChainlinkAggregatorforDAIAddress = mockChainlinkAggregators[0];
+        mockChainlinkAggregatorforUSDCAddress = mockChainlinkAggregators[1];
+        mockChainlinkAggregatorforUSDTAddress = mockChainlinkAggregators[2];
+        mockChainlinkAggregatorforTUSDAddress = mockChainlinkAggregators[3];
+        mockChainlinkAggregatorforMKRAddress = mockChainlinkAggregators[4];
+        mockChainlinkAggregatorforWBTCAddress = mockChainlinkAggregators[8];
+
+        mockChainlinkAggregatorforETHAddress = mockChainlinkAggregators[0];
+        erc20WBTC = await ERC20.at(addressWBTC);
+
+        erc20DAI = await ERC20.at(addressDAI);
+        erc20USDC = await ERC20.at(addressUSDC);
+        erc20USDT = await ERC20.at(addressUSDT);
+        erc20TUSD = await ERC20.at(addressTUSD);
+        erc20MKR = await ERC20.at(addressMKR);
+        cWBTC_addr = await testEngine.tokenInfoRegistry.getCToken(addressWBTC);
+        cDAI_addr = await testEngine.tokenInfoRegistry.getCToken(addressDAI);
+        cUSDC_addr = await testEngine.tokenInfoRegistry.getCToken(addressUSDC);
+        cUSDT_addr = await testEngine.tokenInfoRegistry.getCToken(addressUSDT);
+        cETH_addr = await testEngine.tokenInfoRegistry.getCToken(ETH_ADDRESS);
+        cDAI = await MockCToken.at(cDAI_addr);
+        cUSDC = await MockCToken.at(cUSDC_addr);
+        cUSDT = await MockCToken.at(cUSDT_addr);
+        cWBTC = await MockCToken.at(cWBTC_addr);
+        cETH = await MockCToken.at(cETH_addr);
+
+        mockChainlinkAggregatorforDAI = await MockChainLinkAggregator.at(
+            mockChainlinkAggregatorforDAIAddress
+        );
+        mockChainlinkAggregatorforUSDC = await MockChainLinkAggregator.at(
+            mockChainlinkAggregatorforUSDCAddress
+        );
+        mockChainlinkAggregatorforUSDT = await MockChainLinkAggregator.at(
+            mockChainlinkAggregatorforUSDTAddress
+        );
+        mockChainlinkAggregatorforTUSD = await MockChainLinkAggregator.at(
+            mockChainlinkAggregatorforTUSDAddress
+        );
+        mockChainlinkAggregatorforMKR = await MockChainLinkAggregator.at(
+            mockChainlinkAggregatorforMKRAddress
+        );
+        mockChainlinkAggregatorforETH = await MockChainLinkAggregator.at(
+            mockChainlinkAggregatorforETHAddress
+        );
+        mockChainlinkAggregatorforWBTC = await MockChainLinkAggregator.at(
+            mockChainlinkAggregatorforWBTCAddress
+        );
+
+        ONE_DAI = eighteenPrecision;
+        ONE_USDC = sixPrecision;
+        ZERO = new BN(0);
+    });
+
+    context("onlyOwner", async () => {
+        context("should fail", async function () {
+            this.timeout(0);
+            it("when someone other than the owner calls", async () => {
+                await expectRevert(
+                    tokenInfoRegistry.initialize(globalConfig.address, {
+                        from: user1,
+                    }),
+                    "caller is not the owner"
+                );
+            });
+        });
+
+        context("should succeed", async () => {
+            it("when the owner calls the repective function", async function () {
+                this.timeout(0);
+                await tokenInfoRegistry.initialize(globalConfig.address, {
+                    from: owner,
+                });
+            });
+        });
+    });
+});
