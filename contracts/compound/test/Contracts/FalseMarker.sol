53768e4c75b5e5131966b6e822585346377f160e
JoeBlack220 <348653205@qq.com>
2021-02-24 21:31:10 -0800

    change the way of compiling

--- contracts/compound/test/Contracts/FalseMarker.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.14;
-
-contract FalseMarkerMethodComptroller {
-    bool public constant isComptroller = false;
-}
-
-contract FalseMarkerMethodInterestRateModel {
-    bool public constant isInterestRateModel = false;
-}
