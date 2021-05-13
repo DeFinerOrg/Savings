f002c4baa50e9687f41281a3484945fd21903937
Rishabh <rishabhthaney@gmail.com>
2021-05-13 18:31:43 +0530

    hardhat upgrades

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,15 @@ require("hardhat-typechain");
 require("ts-node/register");
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
 // require("tsconfig-paths/register");
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
 task("balance", "Prints an account's balance")
     .addParam("account", "The account's address")
-    .setAction(async taskArgs => {
+    .setAction(async (taskArgs) => {
         const account = web3.utils.toChecksumAddress(taskArgs.account);
         const balance = await web3.eth.getBalance(account);
 
@@ -35,29 +37,27 @@ module.exports = {
             allowUnlimitedContractSize: true,
             gas: 20000000,
             blockGasLimit: 0x1fffffffffffff,
-
         },
         hardhat: {
             allowUnlimitedContractSize: true,
             gas: 20000000,
             blockGasLimit: 0x1fffffffffffff,
             loggingEnabled: true,
-        }
+        },
     },
-    gasReporter: {
-    },
+    gasReporter: {},
     // This is a sample solc configuration that specifies which version of solc to use
     solidity: {
         version: "0.5.14",
         settings: {
             optimizer: {
-                enabled: true
-            }
-        }
+                enabled: true,
+            },
+        },
     },
 
     typechain: {
         outDir: "./types",
-        target: "truffle-v5"
-    }
+        target: "truffle-v5",
+    },
 };
