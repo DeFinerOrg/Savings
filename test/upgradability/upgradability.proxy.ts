7e8df2749c1a32db15a47abf305cdef84feb7cbc
Rishabh <rishabhthaney@gmail.com>
2021-05-24 19:59:26 +0530

    v1 --> v1.1 upgradability tests

--- test/upgradability/upgradability.proxy.ts
+++ test/upgradability/upgradability.proxy.ts
@@ -109,6 +109,7 @@ contract("SavingAccount() proxy", async (accounts) => {
                 unsafeAllow: ["external-library-linking"],
             });
         });
+
         it("Bank from V1 to latest", async () => {
             // ==================
             // Bank V1
@@ -134,7 +135,7 @@ contract("SavingAccount() proxy", async (accounts) => {
         });
     });
 
-    describe("upgradeability tests from V2 to latest", async () => {
+    describe("upgradeability tests from V1.1 to latest", async () => {
         it("SavingAccount from V1.1 to latest", async () => {
             // ==================
             // SavingAccount V1.1
@@ -248,6 +249,131 @@ contract("SavingAccount() proxy", async (accounts) => {
             // Bank latest Proxy
             // ======================
             const upgradeBank = await upgrades.upgradeProxy(bankProxy.address, Bank);
+        });
+    });
+
+    describe("Upgradability proxy tests from V1 to latest", async () => {
+        it("SavingAccount V1 to V1.1", async () => {
+            // ==================
+            // SavingAccount V1
+            // ==================
+            const UtilsV1 = await ethers.getContractFactory("UtilsV1");
+            const utilsV1 = await UtilsV1.deploy();
+
+            const SavingLibV1 = await ethers.getContractFactory("SavingLibV1", {
+                libraries: {
+                    UtilsV1: utilsV1.address,
+                },
+            });
+            const savingLibV1 = await SavingLibV1.deploy();
+
+            const SavingAccountV1 = await ethers.getContractFactory("SavingAccountV1", {
+                libraries: { SavingLibV1: savingLibV1.address, UtilsV1: utilsV1.address },
+            });
+
+            // ====================
+            // SavingAccount V1.1
+            // ====================
+            const UtilsV1_1 = await ethers.getContractFactory("UtilsV1_1");
+            const utilsV1_1 = await UtilsV1_1.deploy();
+
+            const SavingLibV1_1 = await ethers.getContractFactory("SavingLibV1_1", {
+                libraries: {
+                    UtilsV1_1: utilsV1_1.address,
+                },
+            });
+            const savingLibV1_1 = await SavingLibV1_1.deploy();
+
+            const SavingAccountV1_1 = await ethers.getContractFactory("SavingAccountV1_1", {
+                libraries: { SavingLibV1_1: savingLibV1_1.address, UtilsV1_1: utilsV1_1.address },
+            });
+
+            // ======================
+            // SavingAccount V1 Proxy
+            // ======================
+            const savingAccountProxy = await upgrades.deployProxy(
+                SavingAccountV1,
+                [[], [], DUMMY],
+                { initializer: "initialize", unsafeAllow: ["external-library-linking"] }
+            );
+
+            // ==========================
+            // SavingAccount V1.1 Proxy
+            // ==========================
+            const SAV = await upgrades.upgradeProxy(savingAccountProxy.address, SavingAccountV1_1, {
+                unsafeAllow: ["external-library-linking"],
+            });
+        });
+
+        it("Accounts from V1 to V1.1", async () => {
+            // ==================
+            // Accounts V1
+            // ==================
+            const AccountTokenLibV1 = await ethers.getContractFactory("AccountTokenLibV1");
+            const accountTokenLibV1 = await AccountTokenLibV1.deploy();
+
+            const UtilsV1 = await ethers.getContractFactory("UtilsV1");
+            const utilsV1 = await UtilsV1.deploy();
+
+            const AccountsV1 = await ethers.getContractFactory("AccountsV1", {
+                libraries: {
+                    AccountTokenLibV1: accountTokenLibV1.address,
+                    UtilsV1: utilsV1.address,
+                },
+            });
+
+            // ==================
+            // Accounts V1.1
+            // ==================
+            const AccountTokenLibV1_1 = await ethers.getContractFactory("AccountTokenLibV1_1");
+            const accountTokenLibV1_1 = await AccountTokenLibV1_1.deploy();
+
+            const AccountsV1_1 = await ethers.getContractFactory("AccountsV1_1", {
+                libraries: { AccountTokenLibV1_1: accountTokenLibV1_1.address },
+            });
+
+            // ======================
+            // Accounts V1 Proxy
+            // ======================
+            const accountsProxy = await upgrades.deployProxy(AccountsV1, [DUMMY], {
+                initializer: "initialize",
+                unsafeAllow: ["external-library-linking"],
+            });
+
+            // ======================
+            // Accounts V1.1 Proxy
+            // ======================
+            const upgradeAccounts = await upgrades.upgradeProxy(
+                accountsProxy.address,
+                AccountsV1_1,
+                {
+                    unsafeAllow: ["external-library-linking"],
+                }
+            );
+        });
+
+        it("Bank from V1 to V1.1", async () => {
+            // ==================
+            // Bank V1
+            // ==================
+            const BankV1 = await ethers.getContractFactory("BankV1");
+
+            // ==================
+            // Bank V1.1
+            // ==================
+            const BankV1_1 = await ethers.getContractFactory("BankV1_1");
+
+            // ======================
+            // Bank V1 Proxy
+            // ======================
+            const bankProxy = await upgrades.deployProxy(BankV1, [DUMMY], {
+                initializer: "initialize",
+            });
+
+            // ======================
+            // Bank V1.1 Proxy
+            // ======================
+            const upgradeBank = await upgrades.upgradeProxy(bankProxy.address, BankV1_1);
         });
     });
 });
