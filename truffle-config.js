98c6c25d7437bd8ac9111721c6094cf721cefd41
Jitendra Chittoda <jcchittoda@gmail.com>
2021-06-02 21:57:03 +0530

    try fixing build on CI
    (cherry picked from commit cc6f309511ee8506f9eba2d11429b1063f15d6c2)

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@ require("ts-node/register");
 
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const fs = require("fs");
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
     contracts_build_directory: "./build/contracts",
