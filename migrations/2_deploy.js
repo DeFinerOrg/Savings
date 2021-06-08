fb7e917dc0e51a1ec9577a809420e7087e8c10b4
Jitendra Chittoda <jcchittoda@gmail.com>
2021-06-08 19:20:49 +0530

    use OKExChainData in migration scripts

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,4 +1,4 @@
-var tokenData = require("../test-helpers/tokenData.json");
+var tokenData = require("../test-helpers/OKExChainData.json");
 // var compound = require("../compound-protocol/networks/development.json");
 
 const { BN } = require("@openzeppelin/test-helpers");
