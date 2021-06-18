db9b7af75d73dc5a7bf1e018371d7ff71411b8cb
Jitendra Chittoda <jcchittoda@gmail.com>
2021-06-18 21:41:02 +0530

    add script to read and print the oracle prices for each token

--- contracts/oracle/ExOracle.sol
+++ contracts/oracle/ExOracle.sol
@@ -96,7 +96,7 @@ interface IExOraclePriceData
     //function put(bytes[] calldata messages, bytes[] calldata signatures) external returns (string[] memory keys);
     //function latestRoundData(string calldata priceType, address dataSource) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
     function get(string calldata priceType, address source) external view returns (uint256 price, uint256 timestamp);
-    //function getOffchain(string calldata priceType, address source) external view returns (uint256 price, uint256 timestamp);
+    function getOffchain(string calldata priceType, address source) external view returns (uint256 price, uint256 timestamp);
     //function getCumulativePrice(string calldata priceType, address source) external view returns (uint256 cumulativePrice,uint32 timestamp);
     //function changeSourceRecipient(address _recipient) external;
     //function changeFeederRecipient(address _recipient) external;
---
+++ scripts/readOraclePrices.js
@@ -0,0 +1,56 @@
+var tokenData = require("../test-helpers/OKExChainData.json");
+const hre = require("hardhat");
+const { BN } = require("@openzeppelin/test-helpers/src/setup");
+
+const ONE_OKT = new BN(10).pow(new BN(18));
+const USD_ORACLE_DECIMAL = new BN(10).pow(new BN(6));
+
+const ETH_ADDRESS = "0x000000000000000000000000000000000000000E";
+
+// TESTNET addresses
+const TESTNET_ExOracleAddress = "0x319b07E94eA8288aC0c15b4A24d26577Bec05366";
+const TESTNET_DataSource = "0x41f9950D778425D7B47619e0ad92117E1933918F";
+const TESTNET_TokenRegistry = "0xe59cB8635220b68724b52b8E8b6b39085579D86D";
+const TESTNET_Utils = "0xac9391B80eFbE05c1a0F0a909b365DEf5F59F46A";
+
+async function main() {
+    console.log("Reading testnet prices...");
+    const TokenRegistry = await hre.ethers.getContractFactory("TokenRegistry", {
+        libraries: { Utils: TESTNET_Utils },
+    });
+    const tokenReg = await TokenRegistry.attach(TESTNET_TokenRegistry);
+
+    const exOracle = await hre.ethers.getContractAt(
+        "contracts/oracle/ExOracle.sol:IExOraclePriceData",
+        TESTNET_ExOracleAddress
+    );
+    const result = await exOracle.getOffchain("OKT", TESTNET_DataSource);
+    const oktPrice = result[0];
+    const oktPriceBN = new BN(oktPrice.toString());
+    console.log("OKT Price: $", oktPriceBN.div(USD_ORACLE_DECIMAL).toString());
+
+    const tokens = tokenData.tokens;
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        const price = await tokenReg.priceFromAddress(token.kovan.tokenAddress);
+        const priceInUSD = new BN(price.toString())
+            .mul(oktPriceBN)
+            .div(ONE_OKT)
+            .div(USD_ORACLE_DECIMAL);
+        console.log(token.symbol, " ", price.toString(), "OKT", " = $", priceInUSD.toString());
+    }
+
+    const price = await tokenReg.priceFromAddress(ETH_ADDRESS);
+    const priceInUSD = new BN(price.toString())
+        .mul(oktPriceBN)
+        .div(ONE_OKT)
+        .div(USD_ORACLE_DECIMAL);
+    console.log("ETH ", price.toString(), "OKT", " = $", priceInUSD.toString());
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
