54bd512b30ccd174426b375e74f3234ffebc7963
Jitendra Chittoda <jcchittoda@gmail.com>
2021-08-20 15:01:16 +0530

    improve upgrade script

--- scripts/upgradability/prepare_upgrades.js
+++ scripts/upgradability/prepare_upgrades.js
@@ -1,102 +1,90 @@
-// Previously deployed libraries
-let utilsPrev;
-let savingLibPrev;
-let accountTokenLibPrev;
-
 // Proxy contracts to be upgraded
-let savingAccountPrevProxy;
-let accountsPrevProxy;
-let bankPrevProxy;
+let savingAccountProxy;
+let accountsProxy;
+let bankProxy;
 
 async function main() {
     let network = process.env.HARDHAT_NETWORK;
 
     if (network == "mainnet" || network == "mainnet-fork") {
-        this.utils = "";
-        this.savingLibPrev = "";
-        this.accountTokenLibPrev = "";
-        this.savingAccountPrevProxy = "";
-        this.accountsPrevProxy = "";
-        this.bankPrevProxy = "";
+        savingAccountProxy = "";
+        accountsProxy = "";
+        bankProxy = "";
     } else if (network == "kovan" || network == "kovan-fork") {
-        this.utils = "";
-        this.savingLibPrev = "";
-        this.accountTokenLibPrev = "";
-        this.savingAccountPrevProxy = "";
-        this.accountsPrevProxy = "";
-        this.bankPrevProxy = "";
+        savingAccountProxy = "";
+        accountsProxy = "";
+        bankProxy = "";
     } else if (network == "okex" || network == "okex-fork") {
-        this.utils = "";
-        this.savingLibPrev = "";
-        this.accountTokenLibPrev = "";
-        this.savingAccountPrevProxy = "";
-        this.accountsPrevProxy = "";
-        this.bankPrevProxy = "";
+        savingAccountProxy = "";
+        accountsProxy = "";
+        bankProxy = "";
     }
 
-    // ==========================
-    // Upgrade SavingAccount V1.1
-    // ==========================
-    const Utils = await ethers.getContractFactory("Utils");
-    const utils = await Utils.deploy();
+    await deployBankImpl();
+    await deployAccountsImpl();
+    await deploySavingAccountImpl();
+}
 
-    const SavingLib = await ethers.getContractFactory("SavingLib", {
-        libraries: {
-            Utils: utils.address,
-        },
-    });
-    const savingLib = await SavingLib.deploy();
-
-    const SavingAccount = await ethers.getContractFactory("SavingAccount", {
-        libraries: { SavingLib: savingLib.address, Utils: utils.address },
-    });
-
-    // upgrade proxy
-    const savingAccountLatestAddress = await upgrades.prepareUpgrade(
-        savingAccountV1_1Proxy.address,
-        SavingAccount,
-        {
-            unsafeAllow: ["external-library-linking"],
-        }
-    );
-    console.log("savingAccountLatestAddress", savingAccountLatestAddress);
-    const savingAccount = await SavingAccount.attach(savingAccountLatestAddress);
-
-    // initialize FIN & COMP addresses after upgrade
-    await savingAccount.initFINnCOMPAddresses();
-    let FINAddr = await savingAccount.FIN_ADDR();
-    let COMPAddr = await savingAccount.COMP_ADDR();
-    console.log("FINAddr", FINAddr);
-    console.log("COMPAddr", COMPAddr);
-
-    // =====================
-    // Upgrade Accounts V1.1
-    // =====================
+/**
+ * Deploy Accounts v1.2.0 implementation
+ */
+async function deployAccountsImpl() {
     const AccountTokenLib = await ethers.getContractFactory("AccountTokenLib");
     const accountTokenLib = await AccountTokenLib.deploy();
+    console.log("AccountTokenLib: ", accountTokenLib.address);
 
     const Accounts = await ethers.getContractFactory("Accounts", {
         libraries: { AccountTokenLib: accountTokenLib.address },
     });
 
-    // upgrade proxy
-    const AccountsLatestAddress = await upgrades.prepareUpgrade(
-        accountsV1_1Proxy.address,
-        Accounts,
-        {
-            unsafeAllow: ["external-library-linking"],
-        }
-    );
-    console.log("AccountsLatestAddress", AccountsLatestAddress);
+    const accounts_v_1_2_0_impl = await upgrades.prepareUpgrade(accountsProxy, Accounts, {
+        unsafeAllow: ["external-library-linking"],
+    });
+    console.log("Accounts impl", accounts_v_1_2_0_impl);
+    return accounts_v_1_2_0_impl;
+}
 
-    // =====================
-    // Upgrade Bank V1.1
-    // =====================
+/**
+ * Deploy Bank v1.2.0 implementation
+ */
+async function deployBankImpl() {
     const Bank = await ethers.getContractFactory("Bank");
 
-    // upgrade proxy
-    const BankLatestAddress = await upgrades.prepareUpgrade(bankProxy.address, Bank);
-    console.log("BankLatestAddress", BankLatestAddress);
+    const bank_v_1_2_0_impl = await upgrades.prepareUpgrade(bankProxy, Bank);
+    console.log("Bank impl", bank_v_1_2_0_impl);
+    return bank_v_1_2_0_impl;
+}
+
+/**
+ * Deploy SavingAccount v1.2.0 implementation
+ */
+async function deploySavingAccountImpl() {
+    const Utils = await ethers.getContractFactory("Utils");
+    const utils = await Utils.deploy();
+    console.log("Utils: ", utils.address);
+
+    const SavingLib = await ethers.getContractFactory("SavingLib", {
+        libraries: { Utils: utils.address },
+    });
+    const savingLib = await SavingLib.deploy();
+    console.log("SavingLib: ", savingLib.address);
+
+    const SavingAccount = await ethers.getContractFactory("SavingAccount", {
+        libraries: { SavingLib: savingLib.address, Utils: utils.address },
+    });
+
+    const savingAccount_v_1_2_0_impl = await upgrades.prepareUpgrade(
+        savingAccountProxy,
+        SavingAccount,
+        { unsafeAllow: ["external-library-linking"] }
+    );
+    console.log("SavingAccount impl: ", savingAccount_v_1_2_0_impl);
+    const savingAccount = await SavingAccount.attach(savingAccount_v_1_2_0_impl);
+
+    // To avoid unauthorized initialization,
+    // initialize and pass dummy values to new implementation
+    await savingAccount.init();
+    return savingAccount_v_1_2_0_impl;
 }
 
 main()
