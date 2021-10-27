045b70b33eb4d1d0f9a93273bd78ab9c2f9fcf9e
Rishabh42 <rishabhthaney@gmail.com>
2021-10-27 14:04:42 +0530

    fetch transaction script & okex hardhat config

---
+++ scripts/fetchTransactions.ts
@@ -0,0 +1,55 @@
+import * as t from "../types/truffle-contracts/index";
+import BN from "bn.js";
+
+const SAVING_ACCOUNT = "0xF3c87c005B04a07Dc014e1245f4Cff7A77b6697b";
+const SavingAccount: t.SavingAccountContract = artifacts.require("SavingAccount");
+
+async function main(): Promise<void> {
+    console.log("fetching transactions...");
+
+    // contract deployed 3674866
+
+    await getTransactionsByAccount(SAVING_ACCOUNT, new BN("3674866"), new BN("6319957"));
+    //   await getTransactionsByAccount(SAVING_ACCOUNT, new BN("6272850"), new BN("6272901"));
+}
+
+async function getTransactionsByAccount(myaccount: string, fromBlock: BN, targetBlock: BN) {
+    const savingAccount = await SavingAccount.at(SAVING_ACCOUNT);
+    let batchSize = new BN(2000);
+
+    while (fromBlock.lte(targetBlock)) {
+        const toBlock = fromBlock.add(batchSize);
+        console.log("fromBlock:", fromBlock.toString(), " toBlock:", toBlock.toString());
+        await savingAccount
+            .getPastEvents("allEvents", { fromBlock: fromBlock, toBlock: toBlock })
+            .then((events) => {
+                events.forEach(async (e) => {
+                    // console.log(e.blockNumber, e.transactionHash);
+                    await web3.eth.getTransaction(e.transactionHash).then((tx: any) => {
+                        console.log(
+                            tx.blockNumber,
+                            tx.hash,
+                            tx.from,
+                            tx.to,
+                            tx.gas,
+                            tx.gasPrice,
+                            tx.value,
+                            tx.input
+                        );
+                    });
+                });
+            });
+        fromBlock = fromBlock.add(batchSize).add(new BN(1));
+        const blockDiff = targetBlock.sub(fromBlock);
+
+        if (blockDiff.lte(batchSize)) {
+            batchSize = blockDiff;
+        }
+    }
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error: Error) => {
+        console.error(error);
+        process.exit(1);
+    });
