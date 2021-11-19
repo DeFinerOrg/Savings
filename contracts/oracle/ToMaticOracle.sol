0a4aa3c8f58fed267c7aa7cd2b076491c4ead01c
Jitendra Chittoda <jcchittoda@gmail.com>
2021-11-19 16:53:01 +0530

    add ToMaticOracle which uses chainlink to convert token rates to MATIC

---
+++ contracts/oracle/ToMaticOracle.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.5.14;
+
+import { AggregatorInterface } from "@chainlink/contracts/src/v0.5/dev/AggregatorInterface.sol";
+import { SafeMath} from "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract ToMaticOracle {
+    using SafeMath for uint256;
+    string public tokenPairName,
+    string public maticPairName,
+    string public targetPairName;
+    // example pair DAI/ETH
+    AggregatorInterface public tokenPriceOracle1;
+    // example pair MATIC/ETH
+    AggregatorInterface public maticPriceOracle;
+
+    constructor(
+        string memory _targetPairName,
+        string memory _tokenPairName,
+        AggregatorInterface _tokenPriceOracle1,
+        string memory _maticPairName,
+        AggregatorInterface _maticPriceOracle
+    ) public {
+        targetPairName = _targetPairName;
+
+        // pair1
+        tokenPairName = _tokenPairName;
+        tokenPriceOracle1 = _tokenPriceOracle1;
+
+        // pair2
+        maticPairName = _maticPairName;
+        maticPriceOracle = _maticPriceOracle;
+    }
+
+    function latestAnswer() external view returns (int256) {
+        // 1 DAI rate is represented in ETH
+        uint256 tokenPrice = toUint256(tokenPriceOracle1.latestAnswer());
+        // 1 MATIC rate is repsented in ETH
+        uint256 maticPrice = toUint256(maticPriceOracle.latestAnswer());
+        // number of MATIC in 1 ETH
+        // `10^18 / maticPrice = X`   number of MATIC
+        // `tokenPrice * X` = DAI/MATIC rate
+        return toInt256(tokenPrice.mul(10**18).div(maticPrice));
+
+    }
+
+    function toInt256(uint256 value) internal pure returns (int256) {
+        require(value < uint256(-1), "value doesn\'t fit in 256 bits");
+        return int256(value);
+    }
+
+    function toUint256(int256 value) internal pure returns (uint256) {
+        require(value >= 0, "value must be positive");
+        return uint256(value);
+    }
+}
